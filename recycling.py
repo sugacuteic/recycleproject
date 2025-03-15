0006c000e07fc1eff3598ab55f8a7666e1ed7fee
sugacuteic <zoetang7@outlook.com>
2025-03-15 18:52:01 +0000

    Add files via upload

--- recycling.py
+++ recycling.py
@@ -10,12 +10,33 @@ level = 1
 animations = []
 def draw():
     screen.blit("recyclebg", (0,0))
+    if not gameover:
+        for i in items:
+            i.draw()
+    elif gameover == True:
+        screen.draw.text("Better luck next time! :(", fontsize = 60, color = "Black", center = (WIDTH /2, HEIGHT /2))
+    # elif gameover == "start":
+    #     screen.draw.text("You have to click on the paper bag each level to pass. \n If the items reach the bottom you lose \n ")
+    else: 
+        screen.draw.text("Yay you win! :D", fontsize = 60, color = "Black", center = (WIDTH /2, HEIGHT /2))
+        
+        
+        
+    
 def update():
+    global items 
     if len(items) == 0:
-        make_items(level)
+        items = make_items(level)
+        
         
 
-# def make_items(numberofitems):
+def make_items(numberofitems):
+    create = items_to_create(numberofitems)
+    actors = create_items(create)
+    layout_items(actors)
+    animate_items(actors)
+    return actors
+    
     
     
 def items_to_create(numberofitems):
@@ -31,6 +52,7 @@ def create_items(create):
         actors.append(Actor(i))
     return actors
 
+
 def layout_items(actors):
     number_of_gaps = len(actors) + 1
     gapsize = WIDTH / number_of_gaps
@@ -38,6 +60,41 @@ def layout_items(actors):
     for i,v in enumerate(actors):
         pos = (i + 1) * gapsize
         v.x = pos
+
+def animate_items(actors):
+    global animations 
+    for i in actors:
+        duration = 10 - level
+        i.anchor = ("center", "bottom")
+        animation = animate(i,duration = duration, y = HEIGHT, on_finished = handlegameover)
+        animations.append(animation)
+
+def handlegameover():
+    global gameover
+    gameover = True
+
+def handlegamecomplete():
+    global gameover, level, items, animations
+    if level == 5:
+        gameover = "complete"
+    else:
+        level += 1
+        items = []
+        animations = []
         
+    
+def on_mouse_down(pos):
+    for i in items:
+        if i.collidepoint(pos):
+            if "paper" in i.image:
+                handlegamecomplete()
+            else:
+                handlegameover()
+        else: 
+            handlegameover()
+                
+    
+    
+    
 
 pgzrun.go()
